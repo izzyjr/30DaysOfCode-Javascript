a085535b6d8e8d17274280c7f86e0b5c880e8e0f
Israel Mesa <mesa.israel50@gmail.com>
2017-10-23 02:34:39 +0000

    day sixteen

---
+++ day16.js
@@ -0,0 +1,14 @@
+function main() {
+    var S = readLine();
+    var final = parseInt(S);
+    
+    try{
+        console.log(final.toString().replace('NaN', 'Bad String'));
+    }
+    
+    catch(error){
+        
+    }
+
+    
+}
